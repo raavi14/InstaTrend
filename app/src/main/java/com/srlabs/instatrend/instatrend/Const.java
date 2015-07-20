b14e4391dd5b100aad37d33363398357a2e6ecf4
raavi14 <sandeep.ravi@cerner.com>
2015-07-20 17:34:00 -0500

    Revert "Foundation Code has been added to InstaTrend"
    This reverts commit 0d8708d715c732518d4ee789a1f9ab222c567c7b.

--- app/src/main/java/com/srlabs/instatrend/instatrend/Const.java
+++
@@ -1,9 +0,0 @@
-package com.srlabs.instatrend.instatrend;
-
-/**
- * Created by SR031812 on 7/19/2015.
- */
-public class Const {
-    public static String CONSUMER_KEY="5oW4cerfkkYmXV3C2x2hJsLLi";
-    public static String CONSUMER_SECRET="Gn9euOZtY5nssjT4R9m8TzHqCj7DfDs7320bFpfmiede3Rbibl";
-}
